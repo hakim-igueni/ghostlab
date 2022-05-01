b4c515df2ec9e99e3875b86eb7aa47b8bcdfb621
Narimane LOUNI <narimane.louni@etu.u-paris.fr>
2022-05-01 16:56:59 +0200

    Add class Labyrinth

--- Server/src/Server/Labyrinth.java
+++ Server/src/Server/Labyrinth.java
@@ -1,13 +1,39 @@
 package Server;
 
 public class Labyrinth {
-    private int height;
-    private int width;
+    private int height; //le nombre de lignes
+
+    private int width; //le nombre de colonnes
+
+    private char[][] tab;   //[hauteur][largeur]
 
     public Labyrinth(int height, int width) {
         this.height = height;
         this.width = width;
+        creerlabyrinth(height, width);
     }
+
+    public void creerlabyrinth(int height, int width) {
+        this.tab = new char[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                tab[i][j] = ' ';
+
+            }
+
+        }
+
+        }
+
+    public void afficheLabyrinthe() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(tab[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 
     public int getHeight() {
         return height;
@@ -16,4 +42,4 @@ public class Labyrinth {
     public int getWidth() {
         return width;
     }
-}
+
